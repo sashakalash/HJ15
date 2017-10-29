42f7a75fad2375ee0072d586da8a176ac56dcf7b
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-10-29 14:02:02 +0300

    Corrected handleClick

--- browser/errors/js/ui.js
+++ browser/errors/js/ui.js
@@ -1,6 +1,8 @@
 'use strict';
-function handleClick {
-  openMail();
+function handleClick() {
+	openMail();
 }
-const button = window.getElementById('open');
-button.onclick = handleClick();
+const button = document.getElementById('open');
+button.onclick = handleClick;
+
+   

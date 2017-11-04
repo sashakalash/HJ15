2156a57e4f2f75aa07c823134dfdffafadf41b7e
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-11-04 19:40:33 +0300

    corrected code

--- html-element-collection/drum-machine/drum-machine.js
+++ html-element-collection/drum-machine/drum-machine.js
@@ -2,8 +2,8 @@ var playBtns = document.getElementsByTagName('li');
 function btnClick() {
 	this.getElementsByTagName('audio')[0].play();
 }
-for(var i = 0; i < playBtns.length; i++) {
-	playBtns[i].onclick = btnClick;
+for(const btn of playBtns) {
+	btn.onclick = btnClick;
 }
 	
 

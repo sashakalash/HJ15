da223d3730694f9cecb4732b7686dd5868c99950
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-11-05 00:10:45 +0300

    corrected images folder

--- browser/gallery/slider.js
+++ browser/gallery/slider.js
@@ -3,11 +3,11 @@ const sliderImg = document.getElementById('currentPhoto');
 const next = document.getElementById('nextPhoto');
 const prev = document.getElementById('prevPhoto');
 const imgArr = [
-'breuer-building.jpg', 
-'guggenheim-museum.jpg',
-'headquarters.jpg',
-'IAC.jpg',
-'new-museum.jpg'
+'i/breuer-building.jpg', 
+'i/guggenheim-museum.jpg',
+'i/headquarters.jpg',
+'i/IAC.jpg',
+'i/new-museum.jpg'
 ];
 var step = 0;
 const len = imgArr.length;
@@ -16,14 +16,14 @@ function nextPhoto() {
 	if(step === len) {
 		step = 0;
 	}
-	sliderImg.src = 'i/' + imgArr[step];
+	sliderImg.src = imgArr[step];
 }
 function prevPhoto() {
 	step--;
 	if(step < 0) {
 		step = len - 1;
 	}
-	sliderImg.src = 'i/' + imgArr[step];
+	sliderImg.src = imgArr[step];
 }
 next.onclick = nextPhoto;
 prev.onclick = prevPhoto;

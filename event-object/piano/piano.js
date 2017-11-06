7a9991c85c9d8362229e11f7419b1d66aa3955c5
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-11-06 18:56:14 +0300

    added piano.js

--- event-object/piano/piano.js
+++ event-object/piano/piano.js
@@ -7,54 +7,45 @@ const melodys = [
 'fourth.mp3',
 'fifth.mp3'
 ];
-var playerMelodyCounter = 0;
-var src;
 
+function selectTone(event) {
+	switch (event.type) {
+		case 'keydown':
+			if(event.keyCode == '18'){
+			piano.classList.toggle('higher');
+			} else if(event.keyCode == '16'){
+			piano.classList.toggle('lower');
+			}
+		break;
+		case 'keyup':
+			if(event.keyCode == '18') {
+			piano.classList.remove('higher');
+			} else if(event.keyCode == '16'){
+			piano.classList.remove('lower');
+			}
+		break;
 
-function setMelody() {
-	const player = this.getElementsByTagName('audio')[0];
-	src = melodys[playerMelodyCounter];
-	// console.log(player.src)
-	playerMelodyCounter++;
+	}         	
 }
 
-function selectionTone(event) {
-	if(event.altKey) {
-		piano.classList.remove('middle');
-		piano.classList.add('higher');
-	} else if(event.shiftKey) {
-		piano.classList.remove('middle');
-		piano.classList.add('lower');
-	} 
-}
+document.addEventListener('keydown', selectTone);
+document.addEventListener('keyup', selectTone);
 
-function resetTone(event) {
+function getTone() {
 	if(piano.classList.contains('higher')) {
-		piano.classList.remove('higher');
+		return 'higher/';
 	} else if(piano.classList.contains('lower')) {
-		piano.classList.remove('lower');
+		return 'lower/';
+	} else {
+		return 'middle/';
 	}
-	piano.classList.add('middle');
 }
 
-function playMelody() {
-	const player = this.getElementsByTagName('audio')[0];
-	if(piano.classList.contains('lower')) {
-		player.src = 'sounds/lower/' + src;
-	} else if(piano.classList.contains('higher')) {
-		player.src = 'sounds/higher/' + src;
-	} 
-	player.src = 'sounds/middle/' + src;
-
-	player.play();
-
-}
-
-for(const btn of buttons) {
-	btn.addEventListener('click', setMelody);
-	document.addEventListener('keydown', selectionTone);
-	document.addEventListener('keyup', resetTone);
-	btn.addEventListener('click', playMelody);
-
-}
+Array.from(buttons).forEach((btn, i) => {
+  btn.addEventListener('click', () => {
+    let player = btn.getElementsByTagName('audio')[0];
+    player.src = 'sounds/' + getTone() + melodys[i];
+    player.play();
+  });
+});
 

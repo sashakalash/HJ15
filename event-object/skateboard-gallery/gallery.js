f7fb3e3da8aa376b1bd3724c5c33099f6cb71a8c
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-11-06 20:02:56 +0300

    added gallery.js for skateboards file

---
+++ event-object/skateboard-gallery/gallery.js
@@ -0,0 +1,16 @@
+const gallery = document.getElementsByClassName('gallery-nav')[0];
+const imagesLinks = gallery.getElementsByTagName('a');
+const imgViewer = document.getElementsByClassName('gallery-view')[0];
+
+function chooseImg(event) {
+	for (const link of imagesLinks) {
+		link.classList.remove('gallery-current');
+	}
+	event.preventDefault();
+	this.classList.add('gallery-current');
+	imgViewer.src = this.href;
+}
+
+Array.from(imagesLinks).forEach(link => {
+	link.addEventListener('click', chooseImg);
+});

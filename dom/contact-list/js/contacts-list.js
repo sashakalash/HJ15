063cacf73aaa2a9d1fa8d2926ebdf1dd55067cca
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-11-09 23:22:47 +0300

    added contact-list.js

---
+++ dom/contact-list/js/contacts-list.js
@@ -0,0 +1,28 @@
+const getContacts = () => {
+	try {
+		extractParseContactsList = JSON.parse(loadContacts());
+		return extractParseContactsList;
+	}
+	catch(err) {
+		console.error(err.name, err.message);
+	}
+}
+const contacts = getContacts();
+const createMarkUp =  () => {
+	let markUp = '';
+	let x = '<li><strong></strong></li>';
+	for(let i = 0; i < contacts.length; i++) {
+		markUp += x;
+	}
+	return markUp;
+};
+
+const contactList = document.getElementsByClassName('contacts-list')[0];
+contactList.innerHTML = createMarkUp();
+const profileItems = contactList.querySelectorAll('li');
+
+contacts.forEach((profile, index) => {
+	profileItems[index].dataset.email = profile.email;
+	profileItems[index].dataset.phone =  profile.phone;
+	profileItems[index].querySelector('strong').innerHTML = profile.name;
+});

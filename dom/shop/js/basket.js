94d3758de8af28d7099e7bae0ab3bd20642695a2
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-11-12 13:21:38 +0300

    corrected basket.js

--- dom/shop/js/basket.js
+++ dom/shop/js/basket.js
@@ -1,9 +1,11 @@
 const cartCount = document.querySelector('span#cart-count');
 const cartTotalPrice = document.querySelector('span#cart-total-price');
+cartTotalPrice.dataset.totalSum = +cartTotalPrice.innerHTML;
 
 function addToBasket() {
 	cartCount.innerHTML++;
-	cartTotalPrice.innerHTML = +cartTotalPrice.innerHTML + (+this.dataset.price);  
+	cartTotalPrice.dataset.totalSum = +cartTotalPrice.dataset.totalSum + (+this.dataset.price);
+	cartTotalPrice.innerHTML = getPriceFormatted(cartTotalPrice.dataset.totalSum);
 }
 
 const allBtns = document.querySelectorAll('div#container button');

157f91ff3a496d377fcf38332a6a1dcf6c2535c1
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-11-14 00:56:42 +0300

    corrected tabs-switcher.js

--- xhr/tabs/js/tabs-switcher.js
+++ xhr/tabs/js/tabs-switcher.js
@@ -1,7 +1,5 @@
-document.addEventListener('DOMContentLoaded', openStartTab);
 
 const tabs = document.querySelectorAll('div.tabs a');
-
 const preloader = document.getElementById('preloader');
 const tabContent = document.getElementById('content');
 var tabRequest = new XMLHttpRequest();
@@ -9,14 +7,12 @@ tabRequest.open('GET', 'http://192.168.1.101:8080/components/email-tab.html', tr
 tabRequest.addEventListener('loadstart', tabOnLoadStart);
 tabRequest.addEventListener('load', tabOnLoad);
 
+document.addEventListener('DOMContentLoaded', openStartTab);
 
 function openStartTab() {
-	console.log('domcontentload');
 	tabs[0].classList.add('active');
 	tabRequest.send();
 }
-
-document.addEventListener('DOMContentLoaded', openStartTab);
 
 function pushTab(event) {
 	event.preventDefault();
@@ -41,35 +37,5 @@ function tabOnLoad() {
 for(const tab of tabs) {
 	tab.addEventListener('click', pushTab);
 }
-// // Заголовки табов реализованы тегами <a> в теге <nav>.
-// // вешаем на нажатие a функцию
-
-// // Его необходимо получать асинхронным запросом при активации таба.
-// // на нажатие ссылки повесить событие запроса 
-// // полученный запрос записываем в переменную
-
-
-// Во время ожидания ответа и загрузки данных должен появляться индикатор загрузки.
-// // на onLoadStart вешаем у <div id="preloader"> удали classList.remove('hidden')
-
-// // Адрес, по которому необходимо получить содержимое таба, 
-// // нужно получить из атрибута href ссылки.
-// получи тег a из div tabs
-// запиши в переменную содержимое href аттрибута 
-
-// // Содержимое таба «Email» должно быть показано сразу при открытии страницы.
-// на domcontentload вешаем содержимое таба «Email»
-
-
-// // Переход по ссылке при клике на ней осуществляться не должен.
-// повесь отмену перехода по ссылке при клике на ссыль
-
-// // Содержимое активного таба необходимо поместить в тело тега <div id="content">.
-// то, что лежит в переменной засунь в innerHTML 
-
-// // Чтобы выделить активный таб, ему необходимо добавить класс active.
-// повесь на вызов таба событие classList.add('active')
-
-// // Текущий таб должен быть выделен. Одновременно может быть выбран только один таб.
-// добавь проверку нет ли на другом табе свойства active 
+ 
 

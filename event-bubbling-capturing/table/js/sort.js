defa683ffe5bf755c2fb90f5d86735e78d8415a9
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-11-24 00:55:33 +0300

    + another 1 corretion

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -3,7 +3,7 @@
 function handleTableClick(event) {
     const table = document.querySelector('table');
     table.addEventListener('click', (event) => {
-        if(event.target.dataset.propName) {
+        if(event.target.className == 'prop__name') {
             table.dataset.sortBy = event.target.dataset.propName; 
             if (!event.target.dataset.dir || event.target.dataset.dir === '1') {
                 sortTable(event.target.dataset.propName, - 1);

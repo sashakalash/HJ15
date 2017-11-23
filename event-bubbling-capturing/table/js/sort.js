081fed175680e2ea677f24b5dee04b6f4ecd5521
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-11-24 00:35:40 +0300

    create sorting logic for sort.js

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,18 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+    const tableHeaders = Array.from(document.querySelectorAll('th'));
+    const table = document.querySelector('tr');
+    tableHeaders.forEach((th) => {
+        th.addEventListener('click', (event) => {
+            table.dataset.sortBy = event.currentTarget.dataset.propName;         
+            if (!event.currentTarget.dataset.dir || event.currentTarget.dataset.dir === '1') {
+                sortTable(event.currentTarget.dataset.propName, - 1);
+                event.currentTarget.dataset.dir = '- 1';
+            } else if(event.currentTarget.dataset.dir === '- 1'){
+                sortTable(event.currentTarget.dataset.propName, 1);
+                event.currentTarget.dataset.dir = '1';
+            }
+        });
+    });
 }

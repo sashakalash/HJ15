6f495a9bb88972bfe55dee7dc9ece3e1290cc159
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-11-25 15:19:49 +0300

    changed logic of tabSwitcher()

--- html-document-structure/tabs/js/tabs.js
+++ html-document-structure/tabs/js/tabs.js
@@ -6,41 +6,37 @@ const itemNav = tabsMenu.querySelector('li');
 const demoTab = itemNav.cloneNode(true);
 itemNav.parentNode.removeChild(itemNav);
 
-for(const article of articleList) {
-    if(article.dataset) {
+for (const article of articleList) {
+    if (article.dataset) {
         let tab = demoTab.cloneNode(true);
         tab.firstElementChild.textContent = article.dataset.tabTitle;
         tab.firstElementChild.classList.add(article.dataset.tabIcon);
         tabsMenu.appendChild(tab);
     }
-}
-
-const menuPoints = tabsMenu.children;
-
-for(const article of articleList) {
     article.classList.add('hidden');
 }
 
-tabsMenu.firstElementChild.classList.add('ui-tabs-active');
-articleList[0].classList.remove('hidden');
+const menuPoints = tabsMenu.children;
+var currentTab = tabsMenu.firstElementChild;
+currentTab.classList.add('ui-tabs-active');
+var currentArticle = articleList[0];
+currentArticle.classList.remove('hidden');
 
 function tabSwitcher(event) {
-    if(event.target.classList.contains('ui-tabs-active')) {
-        return;
-    } 
-    let currentArticleClass = this.querySelector('a').textContent; 
-    for(const tab of menuPoints) {
-        tab.classList.remove('ui-tabs-active');
-    }
-    for(const article of articleList) {
-        article.classList.add('hidden');
-        if(article.dataset.tabTitle === currentArticleClass) {
-            article.classList.remove('hidden');
-        }
-    }
-    this.classList.add('ui-tabs-active');
+    if (currentTab != event.currentTarget) {
+        const activedTab = event.currentTarget;
+        const activedArticleName = event.currentTarget.querySelector('a').textContent;
+        currentTab.classList.remove('ui-tabs-active');
+        currentArticle.classList.add('hidden');
+        activedTab.classList.add('ui-tabs-active');
+        Array.from(articleList).find(article => {
+            article.classList.toggle('hidden', article.dataset.tabTitle !== activedArticleName);
+            currentArticle = article;
+        });
+        currentTab = activedTab;
+    }    
 }
 
-for(const tab of menuPoints) {
+for (const tab of menuPoints) {
     tab.addEventListener('click', tabSwitcher);
 }

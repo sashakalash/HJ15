88b79eb0325d4b53a5c4f7d5b8f915d8d3347774
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-11-25 20:23:11 +0300

    create counters logic in counter.js

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,27 @@
 'use strict';
+const counter = document.querySelector('#counter');
+const btns = document.querySelector('.wrap-btns');
+
+if (!localStorage.counter) {
+    localStorage.counter = '0';
+}
+
+counter.textContent = localStorage.counter;
+btns.addEventListener('click', makeCount);
+
+function makeCount(event) {
+    switch (event.target.id) {
+        case 'increment':
+            +localStorage.counter++;
+            break;
+        case 'decrement':
+            if (localStorage.counter != 0) {
+                +localStorage.counter--;
+            }
+            break;
+        case 'reset':
+            localStorage.counter = '0';
+            break;
+    }
+    counter.textContent = localStorage.counter;
+}

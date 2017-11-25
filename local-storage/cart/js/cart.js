28469f1ebd2c26a01c995cd0c4a4c6d4e5261e1a
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-11-26 00:48:40 +0300

    added cart.js

--- local-storage/cart/js/cart.js
+++ local-storage/cart/js/cart.js
@@ -1,1 +1,34 @@
 'use strict';
+const colorAvailbleList = new XMLHttpRequest();
+colorAvailbleList.addEventListener('load', setColorAvailble);
+colorAvailbleList.open('GET', 'https://neto-api.herokuapp.com/cart/colors');
+colorAvailbleList.send();
+
+const sizeAvailbleList = new XMLHttpRequest();
+sizeAvailbleList.addEventListener('load', setSizeAvailble);
+sizeAvailbleList.open('GET', 'https://neto-api.herokuapp.com/cart/sizes');
+sizeAvailbleList.send();
+
+const cartCondition = new XMLHttpRequest();
+cartCondition.addEventListener('load', setCartCondition);
+cartCondition.open('GET', 'https://neto-api.herokuapp.com/cart');
+cartCondition.send();
+
+const colorSwatch = document.querySelector('#colorSwatch');
+const sizeSwatch = document.querySelector('#sizeSwatch');
+const quickCart = document.querySelector('#quick-cart');
+
+
+function setColorAvailble() {
+    console.log(colorAvailbleList.responseText);
+    
+}
+
+function setSizeAvailble() {
+    console.log(sizeAvailbleList.responseText);
+    
+}
+function setCartCondition() {
+    console.log(cartCondition.responseText);
+    
+}

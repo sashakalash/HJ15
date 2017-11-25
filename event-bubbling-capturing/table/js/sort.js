a85f9dd7c1e0d85d23f5c07b29d4b35783943f0b
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-11-25 13:00:34 +0300

    final corrections sort.js

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,17 +1,14 @@
 'use strict';
 
 function handleTableClick(event) {
-    const table = document.querySelector('table');
-    table.addEventListener('click', (event) => {
-        if(event.target.className == 'prop__name') {
-            table.dataset.sortBy = event.target.dataset.propName; 
-            if (!event.target.dataset.dir || event.target.dataset.dir === '1') {
-                sortTable(event.target.dataset.propName, - 1);
-                event.target.dataset.dir = '- 1';
-            } else if(event.target.dataset.dir === '- 1'){
-                sortTable(event.target.dataset.propName, 1);
-                event.target.dataset.dir = '1';
-            }
+    if(event.target.className == 'prop__name') {
+        table.dataset.sortBy = event.target.dataset.propName; 
+        if (!event.target.dataset.dir || event.target.dataset.dir === '1') {
+            sortTable(event.target.dataset.propName, - 1);
+            event.target.dataset.dir = '- 1';
+        } else {
+            sortTable(event.target.dataset.propName, 1);
+            event.target.dataset.dir = '1';
         }
-    });
+    }
 }

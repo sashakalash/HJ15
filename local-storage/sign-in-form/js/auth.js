3b70b780908de30298ccd33ef256c0bffbadff07
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-11-26 00:48:56 +0300

    added auth.js

--- local-storage/sign-in-form/js/auth.js
+++ local-storage/sign-in-form/js/auth.js
@@ -1,1 +1,40 @@
 'use strict';
+const signUpForm = document.querySelector('.sign-up-htm');
+const signUpMessage = signUpForm.querySelector('output.error-message');
+const signUpFormBtn = signUpForm.querySelector('.button');
+const signUpFormData = new FormData(signUpForm);
+const signUpRequest = new XMLHttpRequest();
+signUpRequest.addEventListener('load', signUp);
+signUpRequest.open('POST', 'https://neto-api.herokuapp.com/signup');
+signUpRequest.setRequestHeader('Content-Type', 'application/json');
+
+
+const signInForm = document.querySelector('.sign-in-htm');
+const signInMessage = signInForm.querySelector('output.error-message');
+const signInFormBtn = signInForm.querySelector('.button');
+const signInFormData = new FormData(signInForm);
+const signInRequest = new XMLHttpRequest();
+signInRequest.addEventListener('load', signIn);
+signInRequest.open('POST', 'https://neto-api.herokuapp.com/signin');
+signInRequest.setRequestHeader('Content-Type', 'application/json');
+
+signUpFormBtn.addEventListener('click', signUpRequest.send(JSON.stringify(signUpFormData)));
+signInFormBtn.addEventListener('click', signInRequest.send(JSON.stringify(signInFormData)));
+
+function signUp() {
+	const answer = JSON.parse(signUpRequest.responseText);
+	if (answer.error) {
+		signUpMessage.textContent = answer.message;
+	} else {
+		signUpMessage.textContent = 'Пользователь ' + answer.name + ' успешно зарегистрирован';       
+	}
+}
+
+function signIn() {
+	const answer = JSON.parse(signInRequest.responseText);
+	if (answer.error) {
+		signInMessage.textContent = answer.message;
+	} else {
+		signInMessage.textContent = 'Пользователь ' + answer.name + ' успешно авторизирован';       
+	}
+}

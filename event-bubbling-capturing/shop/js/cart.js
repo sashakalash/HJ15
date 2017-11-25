e1a0b51e1dabf1d359e9f942fc2a82784eb0ef8d
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-11-26 00:48:22 +0300

    corrected unnecessary reloading page

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,12 +1,14 @@
 'use strict';
 const itemsList = document.querySelector('.items-list');
 itemsList.addEventListener('click', clickToBtn);
+
 function clickToBtn(event) {
-    if (event.target.classList.contains('add-to-cart')) {
-        const item = {
-            title: event.target.dataset.title, 
-            price: event.target.dataset.price
-        };
-        addToCart(item);
-    }
+	if (event.target.classList.contains('add-to-cart')) {
+		event.preventDefault();
+		const item = {
+			title: event.target.dataset.title, 
+			price: event.target.dataset.price
+		};
+		addToCart(item);
+	}
 } 

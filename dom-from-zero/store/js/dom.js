4c28203b857f954ef4dcbc91b4734d8f6b1f9240
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-12-15 19:29:54 +0300

    added dom.js

--- dom-from-zero/store/js/dom.js
+++ dom-from-zero/store/js/dom.js
@@ -1,1 +1,29 @@
 'use strict';
+function createElement(node) {
+	if (typeof node === 'object') {
+		const fragment = document.createElement(node.name);
+		Object.keys(node.props).forEach(el => fragment.setAttribute(el, node.props[el]));
+		node.childs.forEach(el => fragment.appendChild(el));
+		return fragment; 
+	}
+	if (typeof node === 'string') {
+		return document.createTextNode(node);
+	}
+}
+
+
+
+// Функция createElement принимает следующие аргументы:
+
+// node, описание DOM-узла, объект.
+// У объекта описания DOM-узла есть следующие свойства:
+
+// name, название, например, h1 или button, строка;
+// props, атрибуты, объект;
+// childs, список дочерних узлов, массив.
+// При вызове с объектом должна создать и вернуть DOM-узел, соответствующий описанию.
+
+// Также функцию createElement можно вызвать со следующими аргументами:
+
+// node, содержимое текстового узла, строка.
+// При вызове со строкой функция должна создать и вернуть текстовый узел.

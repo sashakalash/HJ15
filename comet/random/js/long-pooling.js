5e81ea7fafa373ddbc4036713a3c9eb89f227ef8
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-12-17 00:22:49 +0300

    added long-pooling.js

--- comet/random/js/long-pooling.js
+++ comet/random/js/long-pooling.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const longPooling = document.querySelectorAll('.long-pooling div');
+
+function getNumber() {
+	const xhr = new XMLHttpRequest();
+	xhr.open('GET', 'https://neto-api.herokuapp.com/comet/long-pooling');
+	xhr.addEventListener('load', () => {
+		for (const number of longPooling) {        
+			if (number.classList.contains('flip-it')) {
+				number.classList.remove('flip-it');
+			}
+		}
+		const numberDiv = Array.from(longPooling).find(number => {
+			return number.textContent == xhr.responseText.trim();
+		});
+		numberDiv.classList.add('flip-it');
+	});
+	xhr.send();
+}
+
+setInterval(getNumber, 5000);

759790c48fe7a9d24ec10be75ea62986b804dbd5
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-12-17 00:30:18 +0300

    added websocket.js

--- comet/random/js/websocket.js
+++ comet/random/js/websocket.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const socket = document.querySelectorAll('.websocket div');
+
+const wss = new WebSocket('wss://neto-api.herokuapp.com/comet/websocket'); 
+
+wss.addEventListener('message', (event) => {
+	for (const number of socket) {        
+		if (number.classList.contains('flip-it')) {
+			number.classList.remove('flip-it');
+		}
+	}
+	const numberDiv = Array.from(socket).find(number => {
+		return number.textContent === event.data;
+	});
+	numberDiv.classList.add('flip-it');
+});
+
+
+	
+	

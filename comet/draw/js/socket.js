c0ecbe0c25c975eacdf33a0b3d55fc07ac777212
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-12-16 23:27:05 +0300

    +

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,20 +1,20 @@
 'use strict';
 
 window.editor.addEventListener('update', getCanvas);
-const wss = new WebSocket('wss://neto-api.herokuapp.com/draw');
-wss.addEventListener('open', console.log('open'));  
-wss.addEventListener('message', console.log('1'))
-wss.addEventListener('close', console.log('close'));
 
-function getCanvas(update) {
 
-    
+function getCanvas(update) {    
+	const wss = new WebSocket('wss://neto-api.herokuapp.com/draw'); 
 	const ctx = update.canvas.getContext('2d');
 	const image = ctx.getImageData(0, 0, update.canvas.width, update.canvas.height);
 	const binary = Uint8Array.from(image.data);
-	console.log(binary);
-	wss.send(binary.buffer);
-	// wss.close();
+	wss.addEventListener('open', () => {
+		console.log('open');
+		wss.send(binary.buffer);
+	});  
+	wss.addEventListener('close', e => console.log(e.reason));
+	
+	
 }
 
 

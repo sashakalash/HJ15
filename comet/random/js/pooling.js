9e168a9676b6c8b9c6223330362e5cfa332ebb74
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-12-16 23:27:17 +0300

    add polling.js

--- comet/random/js/pooling.js
+++ comet/random/js/pooling.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const pooling = document.querySelectorAll('.pooling div');
+
+
+function getNumber() {
+	const xhr = new XMLHttpRequest();
+	xhr.open('GET', 'https://neto-api.herokuapp.com/comet/pooling');
+	xhr.addEventListener('load', () => {
+		for (const number of pooling) {        
+			if (number.classList.contains('flip-it')) {
+				number.classList.remove('flip-it');
+			}
+		}
+		const numberDiv = Array.from(pooling).find(number => {
+			return number.textContent === xhr.responseText;
+		});
+		numberDiv.classList.add('flip-it');
+	});
+	xhr.send();
+}
+
+setInterval(getNumber, 5000);

987408320f8b845d93b5f4d71ff8d18f87cbaca8
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-12-17 23:20:47 +0300

    +

--- media/photo-booth/js/script.js
+++ media/photo-booth/js/script.js
@@ -18,38 +18,75 @@ app.appendChild(canvas);
 const ctx = canvas.getContext('2d');
 
 const listPic = document.querySelector('.list');
+const controls = document.querySelector('.controls');
+
 
 navigator.mediaDevices
 	.getUserMedia({video: true, audio: false})
 	    .then((stream) => {
+		controls.classList.add('visible');
 		cameraWindow.src = URL.createObjectURL(stream);
 	})
 	.catch(() => {
 		errorMessage.textContent = 'Не удалось получить доступ к камере';
 	});
 
-function setImg(src) {
-    const figure = document.createElement('figure');
-    const i
-    <figure>
-    <img src="path/to/pic.png">
-    <figcaption>
-      <a href="path/to/pic.png" download="snapshot.png">
-        <i class="material-icons">file_download</i>
-      </a>
-      <a><i class="material-icons">file_upload</i></a>
-      <a><i class="material-icons">delete</i></a>
-    </figcaption>
-  </figure>
-}
+function setImg(imgSrc) {
 
+	function createPicEl(tagName, attributes, text) {
+		const el = document.createElement(tagName);
+		if (typeof attributes === 'object') {
+			Object.keys(attributes).forEach(i => el.setAttribute(i, attributes[i]));
+		}
+		if (typeof text === 'string') {
+			el.textContent = text;
+		}
+		return el;
+	}
+    
+	const figure = createPicEl('figure');
+	const img = createPicEl('img', {'src': imgSrc});
+	const figcaption = createPicEl('figcaption');
+	const snapshot = createPicEl('a', {href: imgSrc, download: 'snapshot.png'});
+	const fileDownloadBtn = createPicEl('i', {class: 'material-icons'}, 'file_download');
+	const firstTagA = createPicEl('a');
+	const secondTagA = createPicEl('a');
+	const fileUploadBtn = createPicEl('i', {class: 'material-icons'}, 'file_upload');
+	const deleteBtn = createPicEl('i', {class: 'material-icons'}, 'delete');
+    
+	firstTagA.appendChild(fileUploadBtn);
+	secondTagA.appendChild(deleteBtn);
+	snapshot.appendChild(fileDownloadBtn);
+	figcaption.appendChild(snapshot);
+	figcaption.appendChild(firstTagA);
+	figcaption.appendChild(secondTagA);
+	figure.appendChild(img);
+	figure.appendChild(figcaption);
+
+	listPic.appendChild(figure);
+}
 
 function getPhoto() {
 	audio.play();
 	canvas.width = cameraWindow.videoWidth;
 	canvas.height = cameraWindow.videoHeight;
-    ctx.drawImage(cameraWindow, 0, 0);
+	ctx.drawImage(cameraWindow, 0, 0);
 	setImg(canvas.toDataURL());
 }
+listPic.addEventListener('click', makeChangeToPhoto);
 
-
+function makeChangeToPhoto(event) {
+	if (event.target.textContent === 'file_upload') {
+		const xhr = new XMLHttpRequest();
+		xhr.open('POST', 'https://neto-api.herokuapp.com/photo-booth', true);
+		xhr.setRequestHeader('Content-Type', 'multipart/form-data');
+		const imgSrc = event.target.parentElement.previousElementSibling.href;        
+		const imgToSend = new FormData();
+		imgToSend.append('image', imgSrc);    
+		xhr.send(imgToSend);  
+	}
+	if (event.target.textContent === 'delete') {
+		const elToRemove = event.target.parentElement.parentElement.parentElement;     
+		listPic.removeChild(elToRemove);
+	}
+}

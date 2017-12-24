82574d319a2eecb8000a0d680e9af5a3d70c810b
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-12-24 20:32:14 +0300

    +

--- canvas/background/js/background.js
+++ canvas/background/js/background.js
@@ -4,29 +4,43 @@ const ctx = canvas.getContext('2d');
 const PI = Math.PI;
 const circleArr = [];
 const crossArr = [];
-
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 const starsSum = Math.round(getRand(50, 200));
 
 function getRand(min, max) {
 	return Math.random() * (max - min) + min;
 }
 
-function nextPoint(x, y, time) {
+function nextPointOne(x, y, time) {
 	return {
 		x: x + Math.sin((50 + x + (time / 10)) / 100) * 3,
 		y: y + Math.sin((45 + x + (time / 10)) / 100) * 4
 	};
 }
 
+function nextPointTwo(x, y, time) {
+	return {
+		x: x + Math.sin((x + (time / 10)) / 100) * 5,
+		y: y + Math.sin((10 + x + (time / 10)) / 100) * 2
+	};
+}
+
+const timeFuncArr = [nextPointOne, nextPointTwo];
+
 class getObj {
 	constructor (xPos = Math.round(getRand(0, canvas.width)), 
 		yPos = Math.round(getRand(0, canvas.height)),
 		size = getRand(0.1, 0.6),
-		angle = getRand(0, 360)) {
+		angle = getRand(0, 360),
+		speed = getRand(-0.2, 0.2),
+		timeFunc = timeFuncArr[Math.round(getRand(0, 1))]) {
 		this.x = xPos;
 		this.y = yPos;
 		this.size = size;
-		this.angle = angle;
+		this.angle = Math.round(angle * PI / 180);
+		this.rotateSpeed = speed;
+		this.timeFunc = timeFunc;
 	} 
 	
 	getCircle() {
@@ -44,8 +58,10 @@ class getObj {
 		ctx.lineTo(this.x + 20 * this.size, this.y);
 		ctx.moveTo(this.x + 10 * this.size, this.y - 10 * this.size);
 		ctx.lineTo(this.x + 10 * this.size, this.y + 10 * this.size);
-		ctx.rotate(PI / this.angle);
+		ctx.translate(this.x, this.y);
+		ctx.rotate(this.angle * this.rotateSpeed);
 		ctx.stroke();
+		ctx.translate(-this.x, -this.y);		
 		return this;
 	}
 	
@@ -68,17 +84,15 @@ function getStaticBackground() {
 
 getStaticBackground();
 
-
 function getNewBackground(array, isCircle) {
 	for (let i = 0; i < array.length; i++) {
 		const item = array[i];
-		const newCoord = nextPoint(item.x, item.y, Date.now());
+		const newCoord = item.timeFunc(item.x, item.y, Date.now());
 		const x = newCoord.x;
 		const y = newCoord.y;
 		const size = item.size;
 		const angle = item.angle;
 		const newItem = new getObj(x, y, size, angle);
-		// console.log(newItem)
 		if (isCircle) {
 			newItem.getCircle();
 		} else {
@@ -94,16 +108,3 @@ setInterval(() => {
 	getNewBackground(crossArr);
 }, 50);
 
-
-
-
-
-
-// При этом вычислять текущее положение каждого объекта необходимо от его изначального положения. 
-// А не от измененного в предыдущий тик. Так как формула расчета положения задаёт колебания вокруг 
-// базовой точки, координаты которой будут первично сгенерированы при создании объекта.
-
-// Фон должен перерисовываться со скоростью 20 кадров в секунду.
-//  Также у крестика необходимо определить 
-// угол поворота от 0 до 360 градусов. Крестик должен медленно поворачиваться 
-// со случайной скоростью в диапазоне -0.2 до 0.2 на тик (один этап перерисовки).

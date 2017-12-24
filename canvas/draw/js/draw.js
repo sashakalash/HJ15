e4346c66590ab818d3d8e63c0dab6bfd33ae143f
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-12-24 14:09:20 +0300

    added draw.js

--- canvas/draw/js/draw.js
+++ canvas/draw/js/draw.js
@@ -1,80 +1,76 @@
-let canvasBody = document.getElementById('draw'),
-	canvas = canvasBody.getContext('2d'),
+'use strict';
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let imgPointsArr = [];
+let isDraw = false;
+let hue = 0;
+let isShift = false;
+let lineRadius = 5;
+let direction = true;
 
-	w = canvasBody.width = window.innerWidth,
-	h = canvasBody.height = window.innerHeight,
-	color = 'hsl(hue,100%,50%)', 
-	tick = 0,
-	newTick = tick + 1,
-	currentHue = 0, 
-	painting = false;
-    lastX = 0, 
-    lastY = 0,
-    direction = true,
-    canvas.lineWidth = 100;
-
-canvas.lineJoin = 'round';
-canvas.lineCap = 'round';
-
-canvasBody.onmousedown = function() {
-	painting = true;
-};
-
-canvasBody.onmouseup = function() {
-	painting = false;
-};
-
-canvasBody.addEventListener('mousedown', (evt) => {
-	const posX = evt.pageX,
-		posY = evt.pageY;
-	[lastX, lastY] = [posX, posY];
+window.addEventListener('resize', () => {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
 });
 
-canvasBody.addEventListener('mouseleave', (evt) => {
-	painting = false;
+canvas.addEventListener('dblclick', () => {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);    
 });
 
-canvasBody.addEventListener('dblclick',  function() {
-	canvas.clearRect(0, 0, w, h);
+canvas.addEventListener('mouseleave',() => {
+	isDraw = false;
+	imgPointsArr = [];    
 });
 
-canvasBody.addEventListener('mousemove', function(e){
-	const posX = e.pageX,
-		posY = e.pageY;
-  	if (painting){
-		++tick;
-    	if (newTick){
-			if (currentHue !== 356){
-				if (!(e.shiftKey)) {
-					currentHue++; 
-				} else {
-					currentHue--;
-				}
-			} else {
-				currentHue = 0;
-			}
-			if(canvas.lineWidth >= 100 || canvas.lineWidth <= 5) {
-				direction = !direction;
-			}
-			if (direction) {
-				canvas.lineWidth++;
-			} else {
-				canvas.lineWidth--;
-			}
-		}
-
-        currentColor = color.replace('hue', currentHue);
-		canvas.strokeStyle  = currentColor;
-		canvas.beginPath();
-		canvas.moveTo(lastX, lastY); 
-		canvas.lineTo(posX, posY);
-		canvas.stroke();
-		[lastX, lastY] = [posX, posY];
+canvas.addEventListener('mousedown', (evt) => {
+	isDraw = true;
+	if (evt.shiftKey) {
+		isShift = true;
 	}
-
 });
 
-window.addEventListener('resize', function(){
-	w = canvasBody.width = window.innerWidth;
-	h = canvasBody.height = window.innerHeight;
+canvas.addEventListener('mousemove', (evt) => {
+	if (!isDraw) {
+		return;
+	}
+	imgPointsArr.push({x: evt.pageX, y: evt.pageY});
+	drawImg(imgPointsArr);
+});
+
+function drawLineBetweenPoints(begin, end) {
+	ctx.quadraticCurveTo(begin.x, begin.y, end.x, end.y);
+}
+
+function drawImg(imgPointsArr) {
+	ctx.beginPath();
+	if (isShift) {
+		hue === 0? hue = 0: hue--;
+	} else {
+		hue === 359? hue = 359: hue++;
+	}
+	if (direction && lineRadius < 100) {
+		lineRadius++;
+	} else {
+		direction = false;
+		lineRadius > 0? lineRadius--: direction = true;
+	}
+	ctx.lineWidth = lineRadius;
+    
+	ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+	ctx.lineJoin = 'round';
+	ctx.lineCap = 'round';
+	ctx.moveTo(imgPointsArr[0].x, imgPointsArr[0].y);
+	for (let i = 1; i < imgPointsArr.length - 1; i++) {
+		drawLineBetweenPoints(imgPointsArr[i], imgPointsArr[i + 1]);
+	}
+	ctx.stroke();
+}
+
+canvas.addEventListener('mouseup', () => {
+	isDraw = false;
+	imgPointsArr = [];
+	isShift = false;
 });

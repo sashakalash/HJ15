cc8fe7d89575c9c3ce49d03c3ce5182f4f51eb1f
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-12-24 17:19:11 +0300

    +

--- canvas/background/js/background.js
+++ canvas/background/js/background.js
@@ -2,6 +2,8 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 const PI = Math.PI;
+const circleArr = [];
+const crossArr = [];
 
 const starsSum = Math.round(getRand(50, 200));
 
@@ -9,69 +11,91 @@ function getRand(min, max) {
 	return Math.random() * (max - min) + min;
 }
 
-var firstTime = true;
-
 function nextPoint(x, y, time) {
-	console.log(x);
 	return {
-	  x: x + Math.sin((50 + x + (time / 10)) / 100) * 3,
-	  y: y + Math.sin((45 + x + (time / 10)) / 100) * 4
+		x: x + Math.sin((50 + x + (time / 10)) / 100) * 3,
+		y: y + Math.sin((45 + x + (time / 10)) / 100) * 4
 	};
-  }
+}
 
-var getStar = function() {
+class getObj {
+	constructor (xPos = Math.round(getRand(0, canvas.width)), 
+		yPos = Math.round(getRand(0, canvas.height)),
+		size = getRand(0.1, 0.6),
+		angle = getRand(0, 360)) {
+		this.x = xPos;
+		this.y = yPos;
+		this.size = size;
+		this.angle = angle;
+	} 
 	
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	let isCircle = true;
-	
-
-	for (let i = 0; i <= starsSum; i++) {
-		if (firstTime) {
-			var x = Math.round(getRand(0, canvas.width));
-			var y = Math.round(getRand(0, canvas.height)); 	
-			
-			// var curX = x;
-			// var curY = y;
-			
-			console.log(x, y, 'in if')
-		}
-			
-					
-		const size = getRand(0.1, 0.6);
-		const angle = getRand(0, 360);
-	 
+	getCircle() {
 		ctx.beginPath();
+		ctx.arc(this.x, this.y, 12 * this.size / 2, 0, 2 * PI);
+		ctx.strokeStyle = 'white';
+		ctx.lineWidth = 5 * this.size;
+		ctx.stroke();
+		return this;
+	}
+	
+	getСross() {
+		ctx.beginPath();			
+		ctx.moveTo(this.x, this.y);
+		ctx.lineTo(this.x + 20 * this.size, this.y);
+		ctx.moveTo(this.x + 10 * this.size, this.y - 10 * this.size);
+		ctx.lineTo(this.x + 10 * this.size, this.y + 10 * this.size);
+		ctx.rotate(PI / this.angle);
+		ctx.stroke();
+		return this;
+	}
+	
+}
+
+function getStaticBackground() {
+	let isCircle = true;
+	for (let i = 0; i <= starsSum; i++) {
 		if (isCircle) {
-			ctx.arc(x, y, 12 * size / 2, 0, 2 * PI);
+			const circle = new getObj;
+			circleArr.push(circle.getCircle());
 			isCircle = false;
 		} else {
-			ctx.moveTo(x, y);
-			ctx.lineTo(x + 20 * size, y);
-			ctx.moveTo(x + 10 * size, y - 10 * size);
-			ctx.lineTo(x + 10 * size, y + 10 * size);
-			ctx.rotate(PI / angle);
-			isCircle = true;	
+			const cross = new getObj;
+			crossArr.push(cross.getСross());
+			isCircle = true;
 		}
-		ctx.strokeStyle = 'white';
-		ctx.lineWidth = 5 * size;
-		ctx.stroke();
-		ctx.closePath();   
+	}	
+}
+
+getStaticBackground();
+
+
+function getNewBackground(array, isCircle) {
+	for (let i = 0; i < array.length; i++) {
+		const item = array[i];
+		const newCoord = nextPoint(item.x, item.y, Date.now());
+		const x = newCoord.x;
+		const y = newCoord.y;
+		const size = item.size;
+		const angle = item.angle;
+		const newItem = new getObj(x, y, size, angle);
+		// console.log(newItem)
+		if (isCircle) {
+			newItem.getCircle();
+		} else {
+			newItem.getСross();
+		}
+		array.splice(i, 1, newItem);
 	}
-	firstTime  = false;
-	console.log(x, 'after for');
+}
 
-	x = nextPoint(x, y, Date.now()).x;
-	y = nextPoint(x, y, Date.now()).y;  
+setInterval(() => {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);	
+	getNewBackground(circleArr, true);
+	getNewBackground(crossArr);
+}, 50);
 
-	console.log(x, typeof x, y, 'new x & y')
-	
-	// requestAnimationFrame(getStar);
-	
-};
 
-getStar();
 
-getStar();
 
 
 
@@ -80,19 +104,6 @@ getStar();
 // базовой точки, координаты которой будут первично сгенерированы при создании объекта.
 
 // Фон должен перерисовываться со скоростью 20 кадров в секунду.
-
-// Для анимации необходимо создать случайное количество объектов в диапазоне о
-// т 50 до 200. При этом количество крестиков и кругов должно быть равным.
-
-// Параметры объектов
-// У всех объектов должен быть определен относительный размер size, случайное число 
-// от 0.1 до 0.6 единиц, который влияет на другие параметры объекта. Все объекты имеют белую обводку. 
-// Толщина обводки равна 5 * size.
-
-// Круг
-// Радиус круга равен 12 * size. Круг закрашивать не нужно.
-
-// Крестик
-// Сторона крестика равна 20 * size. Также у крестика необходимо определить 
+//  Также у крестика необходимо определить 
 // угол поворота от 0 до 360 градусов. Крестик должен медленно поворачиваться 
 // со случайной скоростью в диапазоне -0.2 до 0.2 на тик (один этап перерисовки).

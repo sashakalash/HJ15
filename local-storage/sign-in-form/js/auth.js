a03225accbba4a2937ebd259cfd4d51bb6d4950c
Aleksandr Koloshtivin <kalash.iskander@gmail.com>
2017-12-27 00:22:26 +0300

    corrected form of object to send

--- local-storage/sign-in-form/js/auth.js
+++ local-storage/sign-in-form/js/auth.js
@@ -9,18 +9,18 @@ function auth(event) {
 	const dataField = event.currentTarget;
 	let url = '';
 	let isSignUp = true;
+	let message = 'Пользователь ';
 	if (dataField.className === 'sign-in-htm') {
 		isSignUp = false;
 	}
 	const messField = dataField.querySelector('output.error-message');
-	const form = new FormData(dataField);
 	const dataRequest = new XMLHttpRequest();
 	dataRequest.addEventListener('load', () => {
 		const answer = JSON.parse(dataRequest.responseText);
 		if (answer.error) {
 			messField.textContent = answer.message;
 		} else {
-			let message = 'Пользователь ' + answer.name + ' успешно ';
+			message += answer.name + ' успешно ';
 			message += isSignUp? 'зарегистрирован': 'авторизирован';
 			messField.textContent =  message;       
 		}
@@ -31,53 +31,14 @@ function auth(event) {
 		url = 'https://neto-api.herokuapp.com/signin';
 	}
 	dataRequest.open('POST', url);
-	dataRequest.setRequestHeader('Content-Type', 'application/json');
+	dataRequest.setRequestHeader('Content-Type', 'application/json');	
 	dataField.addEventListener('submit', (event) => {
 		event.preventDefault();
-		dataRequest.send(JSON.stringify(form));	
+		let form = new FormData(event.target);
+		let object = {};
+		form.forEach(function(value, key){
+			object[key] = value;
+		});
+		dataRequest.send(JSON.stringify(object));	
 	});
 }
-
-
-
-
-
-
-// const signInForm = document.querySelector('.sign-in-htm');
-// const signInMessage = signInForm.querySelector('output.error-message');
-// const signInFormData = new FormData(signInForm);
-// const signInRequest = new XMLHttpRequest();
-// signInRequest.addEventListener('load', signIn);
-// signInRequest.open('POST', 'https://neto-api.herokuapp.com/signin');
-// signInRequest.setRequestHeader('Content-Type', 'application/json');
-
-// signUpForm.addEventListener('submit', sendSingUpRequest);
-// signInForm.addEventListener('submit', sendSingInRequest);
-
-// function sendSingUpRequest(event) {
-// 	event.preventDefault();
-// 	signUpRequest.send(JSON.stringify(signUpFormData));	
-// }
-
-// function sendSingInRequest(event) {
-// 	event.preventDefault();
-// 	signInRequest.send(JSON.stringify(signInFormData));
-// }
-
-// function signUp() {
-// 	const answer = JSON.parse(signUpRequest.responseText);
-// 	if (answer.error) {
-// 		signUpMessage.textContent = answer.message;
-// 	} else {
-// 		signUpMessage.textContent = 'Пользователь ' + answer.name + ' успешно зарегистрирован';       
-// 	}
-// }
-
-// function signIn() {
-// 	const answer = JSON.parse(signInRequest.responseText);
-// 	if (answer.error) {
-// 		signInMessage.textContent = answer.message;
-// 	} else {
-// 		signInMessage.textContent = 'Пользователь ' + answer.name + ' успешно авторизирован';       
-// 	}
-// }
